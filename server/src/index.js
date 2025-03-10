27630f326419907a52b94bd22f0fd4a856030638
kaveesha nipun <kaveeshanipu0@gmail.com>
2025-03-11 00:48:09 +0530

    API setting

--- server/src/index.js
+++ server/src/index.js
@@ -1,1 +1,31 @@
-console.log("hello world k");
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios");
+
+const app = express();
+
+//mmiddle wares 
+app.use(express.json());
+app.use(cors());
+
+
+//all currencys
+app.get("/getAllCurrencies" , (req, res)=>{
+    const nameURL= 'https://openexchangerates.org/api/currencies.json?app_id=034852e74bf7417f8ee5fdd540599a75';
+    
+   
+    
+    try{
+        const namesResponce = awaitaxios.get(nameURL);
+        const nameData = namesResponce.data; 
+        return res.json(nameData);
+   
+
+        }catch(err){console.error(err);
+        }
+})
+//listen to a port 
+
+app.listen( 5000 , ()=>{
+    console.log("SEVER STARTED");
+})
